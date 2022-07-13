3cadfa7f1df3171def8bff3c93ef04649fdf4470
Linneker <linneker.blytner@gmail.com>
2022-07-12 23:36:47 -0300

    erros

--- src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Payment/PagamentoService.cs
+++ src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Payment/PagamentoService.cs
@@ -1,38 +1,20 @@
-<<<<<<<< HEAD:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Payment/PagamentoService.cs
-﻿using acme.sistemas.compracoletiva.core.Interfaces.Service.Payment;
-using acme.sistemas.compracoletiva.domain.Entity.Utils;
-using acme.sistemas.compracoletiva.domain.Interfaces.Repository.Payment;
-using AutoMapper;
-========
-﻿using acme.sistemas.compracoletiva.core.Interfaces.Service.Utils;
 using acme.sistemas.compracoletiva.domain.Entity.Utils;
 using acme.sistemas.compracoletiva.domain.Interfaces.Repository.Utils;
->>>>>>>> d92261d239de20e7456eff4e939cb2f22c03fb79:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Utils/PagamentoService.cs
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using acme.sistemas.compracoletiva.service.Interfaces.Service.Utils;
+using AutoMapper;
 
-<<<<<<<< HEAD:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Payment/PagamentoService.cs
-namespace acme.sistemas.compracoletiva.service.Service.Payment
-========
 namespace acme.sistemas.compracoletiva.service.Service.Utils
->>>>>>>> d92261d239de20e7456eff4e939cb2f22c03fb79:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Utils/PagamentoService.cs
 {
-    internal class PagamentoService : BaseService<Pagamento>, IPagamentoService
+    public class PagamentoService : BaseService<Pagamento>, IPagamentoService
     {
         private readonly IPagamentoRepository _pagamentoRepository;
-        private readonly IMapper mapper;
+        private readonly IMapper _mapper;
 
-<<<<<<<< HEAD:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Payment/PagamentoService.cs
-        public PagamentoService(IPagamentoRepository pagamentoRepository, IMapper mapper) : base(pagamentoRepository)
-========
-        public PagamentoService(IPagamentoRepository pagamentoRepository) : base(pagamentoRepository)
->>>>>>>> d92261d239de20e7456eff4e939cb2f22c03fb79:src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Utils/PagamentoService.cs
+        public PagamentoService(IPagamentoRepository pagamentoRepository, IMapper mapper, IPagamentoRepository pagamentoRepository, IMapper mapper) : this(pagamentoRepository)
         {
-            _pagamentoRepository = pagamentoRepository;
             this.mapper = mapper;
+            this._pagamentoRepository = pagamentoRepository;
+            _mapper = mapper;
         }
     }
 }
--- src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Utils/PagamentoService.cs
+++ src/acme.sistemas.compracoletiva/src/Config/acme.sistemas.compracoletiva.service/Service/Utils/PagamentoService.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace acme.sistemas.compracoletiva.service.Service.Utils
 {
